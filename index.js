28dfb00b90665294d444b1b60102ac894123579a
Hassan Mostafa <48831222+HassanMostafaa@users.noreply.github.com>
2022-06-09 06:27:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ server.listen(server_port, server_host, () => {
 });
 
 var http = require("http");
-setInterval(function () {
-  http.get("https://profiled-notes-json-server.herokuapp.com/users");
-}, 1000000); 
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
